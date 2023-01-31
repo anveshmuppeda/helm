444f27f6f4700ab8c1fc74830f9662ec786ddd75
anveshmuppeda <muppedaanvesh@gmail.com>
2023-01-31 00:40:02 -0600

    Updating notes and materials

---
+++ prometheus/prometheus.sh
@@ -0,0 +1,7 @@
+
+# add the prometheus-community repo as follows:
+$ helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+#You can then run "helm search" to see the available charts charts.
+$ helm search repo prometheus-community
+
